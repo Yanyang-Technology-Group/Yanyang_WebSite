6c61ecd866e7292d476becd396f3d6888845fb5d
VasilyZa <450270247@qq.com>
2026-06-28 13:30:10 +0800

    feat: add Tailwind CSS with design tokens

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import 'tdesign-react/es/style/index.css'
-import './style.css'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
