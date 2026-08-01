1f140d5c3f37d94a1efd7011dbf8b0c06167a87a
shenshui6 <kk160613646@163.com>
2026-08-01 16:56:58 +0800

    add: Auto Add File of Page Download

--- src/services/github.js
+++ src/services/github.js
@@ -1,5 +1,5 @@
-export async function getData(env) {
-  const url = `https://api.github.com/repos/${env.REPO_OWNER}/${env.REPO_NAME}/contents/downloads.json`
+export async function getData(env, filename) {
+  const url = `https://api.github.com/repos/${env.REPO_OWNER}/${env.REPO_NAME}/contents/${filename}`
 
   const res = await fetch(url, {
     headers: {
@@ -14,7 +14,6 @@ export async function getData(env) {
   }
 
   const data = await res.json()
-
   const binary = atob(data.content)
   const bytes = new Uint8Array(binary.length)
   for (let i = 0; i < binary.length; i++) {
@@ -22,6 +21,26 @@ export async function getData(env) {
   }
   const decoder = new TextDecoder('utf-8')
   const text = decoder.decode(bytes)
-
   return JSON.parse(text)
+}
+
+export async function getDownkey(env) {
+  return getData(env, 'downkey.json')
+}
+
+export async function getModpacks(env) {
+  return getData(env, 'modpack.json')
+}
+
+export async function getJava(env) {
+  return getData(env, 'javajdk.json')
+}
+
+export async function getLaunchers(env) {
+  return getData(env, 'launcher.json')
+}
+
+export async function verifyPassword(password, env) {
+  const data = await getDownkey(env)
+  return data.password === password
 }
